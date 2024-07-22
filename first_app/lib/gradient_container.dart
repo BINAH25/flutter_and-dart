0de7d24fff3ab39f2cee095a5cba77772ecc2bd0
visteen <visteen192@gmail.com>
2024-07-22 17:33:27 +0000

    reusable component added

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText("Hello Louis"),
+      ),
+    );
+  }
+}
+
+class StyledText extends StatelessWidget {
+  const StyledText(this.text, {super.key});
+  final String text;
+
+  @override
+  Widget build(context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 30.0,
+      ),
+    );
+  }
+}
