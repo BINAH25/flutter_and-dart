145639dcac4c7b54ec9815747b5ccfa9fd2b7235
visteen <visteen192@gmail.com>
2024-07-22 16:51:51 +0000

    separate class created

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,30 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 97, 27, 216),
-                Color.fromARGB(255, 48, 25, 89)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello world!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 97, 27, 216),
+            Color.fromARGB(255, 48, 25, 89)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
 }
