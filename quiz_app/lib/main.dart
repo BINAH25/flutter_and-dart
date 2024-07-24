f35f6e5eca10951c6075ba5f4b7e5a67bbc4d5c6
visteen <visteen192@gmail.com>
2024-07-24 11:23:19 +0000

    quiz screen added

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/gradient_container.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(
-          Colors.deepPurple,
-          Color.fromARGB(255, 42, 8, 100),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
